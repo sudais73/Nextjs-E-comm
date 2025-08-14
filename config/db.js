665eab75561ad27cd90fa7af2687a6e3a8618f2c
sudais73 <sudaisman193@gmail.com>
2025-08-14 15:40:30 +0300

    mongodb updated

--- config/db.js
+++ config/db.js
@@ -2,21 +2,25 @@ import mongoose from "mongoose";
 
 let cached = global.mongoose;
 
-if(!cached){
-    cached = global.mongoose = {conn:null, promise:null}
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
-    if(cached.conn){
-        return cached.conn;
-    }
-    if(!cached.promise){
-        const opts={
-        bufferCommands:false}
-        cached.promise = (await mongoose.connect(process.env.MONGODB_URI, opts)).then(mongoose)
-    return mongoose
-    }
-    cached.conn = await cached.promise
-    return cached.con
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = { bufferCommands: false };
+
+    cached.promise = mongoose
+      .connect(process.env.MONGODB_URI, opts)
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
+
 export default connectDB;
